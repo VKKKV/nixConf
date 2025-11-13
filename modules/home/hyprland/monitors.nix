c30376a5292f196f753a77c934e7159207627b28
vkkkv <104302767+VKKKV@users.noreply.github.com>
2025-11-13 16:53:32 +0800

    hyprland

--- modules/home/hyprland/monitors.nix
+++ modules/home/hyprland/monitors.nix
@@ -2,10 +2,6 @@
 {
   wayland.windowManager.hyprland = {
     settings.monitor = [ ",preferred,auto,auto" ];
-
-    extraConfig = ''
-      source = ~/.config/hypr/monitors.conf
-    '';
   };
 
   home.packages = with pkgs; [ nwg-displays ];
