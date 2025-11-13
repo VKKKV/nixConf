05e5f91c77cbb53d529bd32dac43587cbb031f4f
vkkkv <vkkkv@example.com>
2025-11-13 21:47:48 +0800

    laptop tlp

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -19,14 +19,14 @@
       enable = true;
       keyboards = {
         default = {
-        ids = [ "*" ];
-        settings = {
-          main = {
-            capslock = "overload(control,esc)";
+          ids = [ "*" ];
+          settings = {
+            main = {
+              capslock = "overload(control,esc)";
+            };
           };
         };
       };
-    };
     };
 
     upower = {
@@ -38,27 +38,39 @@
     };
 
     tlp.settings = {
-      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 80;
 
       CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 1;
+      CPU_BOOST_ON_BAT = 0;
 
       CPU_HWP_DYN_BOOST_ON_AC = 1;
-      CPU_HWP_DYN_BOOST_ON_BAT = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
 
       PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "performance";
+      PLATFORM_PROFILE_ON_BAT = "powersave";
 
       INTEL_GPU_MIN_FREQ_ON_AC = 500;
-      INTEL_GPU_MIN_FREQ_ON_BAT = 500;
+      INTEL_GPU_MIN_FREQ_ON_BAT = 200;
+
       # INTEL_GPU_MAX_FREQ_ON_AC=0;
       # INTEL_GPU_MAX_FREQ_ON_BAT=0;
       # INTEL_GPU_BOOST_FREQ_ON_AC=0;
       # INTEL_GPU_BOOST_FREQ_ON_BAT=0;
 
-      # PCIE_ASPM_ON_AC = "default";
-      # PCIE_ASPM_ON_BAT = "powersupersave";
+      PCIE_ASPM_ON_AC = "default";
+      PCIE_ASPM_ON_BAT = "powersupersave";
     };
   };
   hardware = {
