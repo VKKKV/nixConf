c56bbf78c5a8f9b0717ca1c3ab4f3660e7880286
vkkkv <104302767+VKKKV@users.noreply.github.com>
2025-11-13 21:50:10 +0800

    format

--- modules/home/packages/cli.nix
+++ modules/home/packages/cli.nix
@@ -2,46 +2,48 @@
 {
   home.packages = with pkgs; [
     ## Better core utils
-    duf                               # disk information
-    eza                               # ls replacement
-    fd                                # find replacement
-    gping                             # ping with a graph
-    gtrash                            # rm replacement, put deleted files in system trash
-    hexyl                             # hex viewer
-    man-pages                         # extra man pages
-    ncdu                              # disk space
-    ripgrep                           # grep replacement
+    zsh
+    duf # disk information
+    eza # ls replacement
+    fd # find replacement
+    gping # ping with a graph
+    gtrash # rm replacement, put deleted files in system trash
+    hexyl # hex viewer
+    man-pages # extra man pages
+    ncdu # disk space
+    ripgrep # grep replacement
     # replace with tealdeer
     # tldr
 
     ## Tools / useful cli
-    aoc-cli                           # Advent of Code command-line tool
+    aoc-cli # Advent of Code command-line tool
     asciinema
     asciinema-agg
     binsider
-    bitwise                           # cli tool for bit / hex manipulation
-    broot                             # tree files view
-    caligula                          # User-friendly, lightweight TUI for disk imaging
-    hyperfine                         # benchmarking tool
-    pastel                            # cli to manipulate colors
-    swappy                            # snapshot editing tool
-    tdf                               # cli pdf viewer
-    tokei                             # project line counter
-    translate-shell                   # cli translator
+    bitwise # cli tool for bit / hex manipulation
+    broot # tree files view
+    caligula # User-friendly, lightweight TUI for disk imaging
+    hyperfine # benchmarking tool
+    pastel # cli to manipulate colors
+    swappy # snapshot editing tool
+    tdf # cli pdf viewer
+    tokei # project line counter
+    translate-shell # cli translator
     woomer
     yt-dlp-light
 
     ## TUI
-    epy                               # ebook reader
-    gtt                               # google translate TUI
+    epy # ebook reader
+    gtt # google translate TUI
     programmer-calculator
-    toipe                             # typing test in the terminal
-    ttyper                            # cli typing test
+    toipe # typing test in the terminal
+    ttyper # cli typing test
 
     ## Monitoring / fetch
     htop
-    onefetch                          # fetch utility for git repo
-    wavemon                           # monitoring for wireless network devices
+    onefetch # fetch utility for git repo
+    fastfetch
+    wavemon # monitoring for wireless network devices
 
     ## Fun / screensaver
     asciiquarium-transparent
@@ -63,22 +65,22 @@
     mpv
 
     ## Utilities
-    entr                              # perform action when file change
+    entr # perform action when file change
     ffmpeg
-    file                              # Show file information
-    jq                                # JSON processor
+    file # Show file information
+    jq # JSON processor
     killall
     libnotify
     mimeo
     openssl
-    pamixer                           # pulseaudio command line mixer
-    playerctl                         # controller for media players
+    pamixer # pulseaudio command line mixer
+    playerctl # controller for media players
     poweralertd
-    udiskie                           # Automounter for removable media
+    udiskie # Automounter for removable media
     unzip
     p7zip
     wget
-    wl-clipboard                      # clipboard utils for wayland (wl-copy, wl-paste)
+    wl-clipboard # clipboard utils for wayland (wl-copy, wl-paste)
     xdg-utils
     cloc
 
