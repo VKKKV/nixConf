d21e6f839e3bbfe222f560bde8f6cf94df7d2205
vkkkv <104302767+VKKKV@users.noreply.github.com>
2025-11-14 20:50:19 +0800

    change core wayland conf

---
+++ modules/core/xdg.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+{
+  programs.hyprland = {
+    enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common = {
+        default = [
+          "gnome"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+        "org.freedesktop.impl.portal.Screenshot" = "gnome";
+        "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
+      };
+    };
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
+    ];
+  };
+}
