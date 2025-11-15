5b8402eff7ec78e3340549c7595e16d739c768d2
vkkkv <104302767+VKKKV@users.noreply.github.com>
2025-11-16 04:04:05 +0900

    up fish

--- modules/home/termimal/fish/default.nix
+++ modules/home/termimal/fish/default.nix
@@ -2,11 +2,25 @@
 {
   programs.fish = {
     enable = true;
-  };
 
-  xdg.configFile."fish" = {
-    source = ./config;
-    recursive = true;
-    executable = true;
+    interactiveShellInit = ''
+      set -U fish_greeting ""
+    '';
+
+    shellAliases = {
+     cp = "cp -r";
+     diff = "diff --color=auto";
+     dt = "date \"+%Y-%m-%d %H:%M:%S\"";
+     e = "exit";
+     grep = "grep --color=auto";
+     ip = "ip -color=auto";
+     mkdir = "mkdir -p";
+     gpg = "gpg --pinentry-mode loopback";
+     ta = "tmux attach; or tmux";
+     up = "paru -Syu";
+     zathura = "zathura --fork";
+     mpvhdr = "ENABLE_HDR_WSI=1 mpv --vo=gpu-next --target-colorspace-hint --gpu-api=vulkan --gpu-context=waylandvk";
+     dd = "dd bs=4M conv=fsync oflag=direct status=progress";
+    };
   };
 }
