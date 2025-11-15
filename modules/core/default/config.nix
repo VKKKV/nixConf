3713d168ce295adecb9f9ac4d487cb608e45682f
vkkkv <104302767+VKKKV@users.noreply.github.com>
2025-11-16 02:46:55 +0900

    up hardware

--- modules/core/default/config.nix
+++ modules/core/default/config.nix
@@ -28,20 +28,6 @@
     memoryPercent = 25;
   };
 
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      libva
-      libva-vdpau-driver
-      libvdpau-va-gl
-      mesa
-    ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [
-      libva-vdpau-driver
-      libvdpau-va-gl
-    ];
-  };
-
   time = {
     timeZone = lib.mkDefault "Asia/Tokyo";
     hardwareClockInLocalTime = lib.mkDefault true;
---
+++ modules/core/default/hardware.nix
@@ -0,0 +1,37 @@
+{ pkgs, ...}:
+{
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        libva
+        libva-vdpau-driver
+        libvdpau-va-gl
+        mesa
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        libva-vdpau-driver
+        libvdpau-va-gl
+      ];
+    };
+
+    bluetooth = {
+      enable = true;
+      package = pkgs.bluez5-experimental;
+      settings = {
+        # make Xbox Series X controller work
+        General = {
+          Experimental = true;
+          FastConnectable = true;
+          powerOnBoot = false;
+          JustWorksRepairing = "always";
+          Privacy = "device";
+        };
+      };
+    };
+  };
+
+  services.fwupd.enable = true;
+  boot.extraModprobeConfig = ''options bluetooth disable_ertm=1 '';
+  systemd.user.services.telephony_client.enable = false;
+}
