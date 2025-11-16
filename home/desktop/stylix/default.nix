b8535c792386c9e12e9f953f2bf1cfd9cd785f24
vkkkv <vkkkv@example.com>
2025-11-16 20:00:05 +0900

    chore
    chore

--- home/desktop/stylix/default.nix
+++ home/desktop/stylix/default.nix
@@ -17,6 +17,6 @@
     # url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
     # hash = "sha256-enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
     # };
-    image = ./gruvbox/forest_bridge.jpg;
+    image = ../../../gruvbox/forest_bridge.jpg;
   };
 }
