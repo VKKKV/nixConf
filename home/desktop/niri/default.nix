85df27b0c39664d45980f13954ebf57f77039a43
vkkkv <104302767+VKKKV@users.noreply.github.com>
2025-11-22 18:50:23 +0800

    chore
    chore
    
    chore
    
    chore

--- home/desktop/niri/default.nix
+++ home/desktop/niri/default.nix
@@ -15,38 +15,37 @@
     pwvucontrol
     cliphist
 
-  blueberry           # 蓝牙图形界面
-  gnome.gnome-calendar
-  gnome.gnome-clocks
-  pavucontrol         # PulseAudio 控制面板
-  fuzzel              # 应用程序启动器
-  waypaper            # 背景图片选择器
-  wlogout             # 电源菜单界面
-  # 系统工具
-  power-profiles-daemon
-  upower              # 电池状态
-  playerctl           # MPRIS 媒体控制
-  libnotify           # 通知工具
-  swaynotificationcenter  # 通知中心
-  swaylock            # 屏幕锁定器
-  ddcutil             # 显示器亮度控制
-  wl-clipboard        # Wayland 剪贴板 (wl-copy)
-  
-  # Wayland 工具
-  grim                # 截图捕获
-  slurp               # 区域选择
-  hyprpicker          # 颜色选择器
-  cava                # 音频可视化器
-  # 终端工具
-  networkmanager      # 用于 nmtui
-  # 音频
-  wireplumber         # wpctl 音频控制
-  # 蓝牙
-  bluez               # 蓝牙协议栈
-  bluez-tools
-  # 守护进程
-  blueman             # 蓝牙管理
-  udisks2             # 磁盘管理用于托盘图标
+    blueberry # 蓝牙图形界面
+    gnome-calendar
+    gnome-clocks
+    pavucontrol # PulseAudio 控制面板
+    fuzzel # 应用程序启动器
+    waypaper # 背景图片选择器
+    wlogout # 电源菜单界面
+    # 系统工具
+    power-profiles-daemon
+    upower # 电池状态
+    playerctl # MPRIS 媒体控制
+    libnotify # 通知工具
+    swaynotificationcenter # 通知中心
+    ddcutil # 显示器亮度控制
+    wl-clipboard # Wayland 剪贴板 (wl-copy)
+
+    # Wayland 工具
+    grim # 截图捕获
+    slurp # 区域选择
+    hyprpicker # 颜色选择器
+    cava # 音频可视化器
+    # 终端工具
+    networkmanager # 用于 nmtui
+    # 音频
+    wireplumber # wpctl 音频控制
+    # 蓝牙
+    bluez # 蓝牙协议栈
+    bluez-tools
+    # 守护进程
+    blueman # 蓝牙管理
+    udisks2 # 磁盘管理用于托盘图标
   ];
 
   home.sessionVariables = {
