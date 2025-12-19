bc1301e79e9475f27e3d7cc92933371979854811
vkkkv <104302767+VKKKV@users.noreply.github.com>
2025-12-19 13:37:39 +0800

    i suck at i18n

--- system/common/core.nix
+++ system/common/core.nix
@@ -11,21 +11,7 @@
     hardwareClockInLocalTime = true;
   };
 
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    supportedLocales = [ "en_US.UTF-8" "es_US.UTF-8" ];
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
+  i18n.defaultLocale = "en_US.UTF-8";
 
   zramSwap = {
     enable = true;
