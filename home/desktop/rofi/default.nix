a0cdd678925a8a60f14911c37884c36f47e4c1b2
vkkkv <104302767+VKKKV@users.noreply.github.com>
2025-12-19 13:47:08 +0800

    update rofi

--- home/desktop/rofi/default.nix
+++ home/desktop/rofi/default.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [rofi];
-  xdg.configFile."rofi/theme.rasi".source = ./theme.rasi;
-  xdg.configFile."rofi/config.rasi".source = ./config.rasi;
-  xdg.configFile."rofi/powermenu-theme.rasi".source = ./powermenu-theme.rasi;
+  xdg.configFile."rofi" = {
+    source = ./config;
+    recursive = true;
+    executable = true;
+  };
 }
